09cf9ee6c7314c7061ab87e41fa9019f2862b3b0
Austin <austin.johnson.sd@gmail.com>
2025-10-14 19:37:09 -0700

    Adding counter to navvar

--- imports/ui/Header.jsx
+++ imports/ui/Header.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Roles } from "meteor/roles";
+import Badge from 'react-bootstrap/Badge';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useTracker } from 'meteor/react-meteor-data';
+
+import { CarsCollection } from '/imports/api/CarsCollection.js';
+import { ShuttleCollection } from '/imports/api/ShuttleCollection.js';
+import { TasksCollection } from '/imports/api/TasksCollection.js';
 
 function Header() {
   const logout = () => {
     Meteor.logout();
   };
+
+  var numCars = useTracker(() => CarsCollection.find({ company: Meteor.user().profile.company }).fetch());
+  var numShuttles = useTracker(() => ShuttleCollection.find({ company: Meteor.user().profile.company }).fetch());
+  var numTasks = useTracker(() => TasksCollection.find({ company: Meteor.user().profile.company }).fetch());
 
   return (
     <Navbar className="bg-body-secondary" expand="lg">
@@ -17,9 +27,9 @@ function Header() {
       <Container>
 
         <Nav className="me-auto">
-          <Nav.Link href="/cars">Cars</Nav.Link>
-          <Nav.Link href="/shuttle">Shuttle</Nav.Link>
-          <Nav.Link href="/tasks">Tasks</Nav.Link>
+          <Nav.Link href="/cars">Cars <Badge bg="info">{numCars.length}</Badge></Nav.Link>
+          <Nav.Link href="/shuttle">Shuttle <Badge bg="info">{numShuttles.length}</Badge></Nav.Link>
+          <Nav.Link href="/tasks">Tasks <Badge bg="info">{numTasks.length}</Badge></Nav.Link>
           {Roles.userIsInRole(Meteor.user(), 'admin') ? (
             <Nav.Link href="/admin">Admin</Nav.Link>
           ) : null}
